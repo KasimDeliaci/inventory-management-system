85916492e8d5a36887b06db2747a536c7082ab0f
Eren GURELI <erengureli10@gmail.com>
2025-08-27 14:06:05 +0300

    fix(product): empty string validation

--- services/inventory-service/src/main/java/com/petek/inventory_service/product/ProductRequest.java
+++ services/inventory-service/src/main/java/com/petek/inventory_service/product/ProductRequest.java
@@ -4,19 +4,23 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record ProductRequest (
     Long productId,
 
     @NotNull(message = "Product name is required")
+    @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
     String productName,
 
     String description,
 
     @NotNull(message = "Category is required")
+    @Size(min = 2, max = 100, message = "Category must be between 2 and 100 characters")
     String category,
 
     @NotNull(message = "Unit of measure is required")
+    @Size(min = 2, max = 100, message = "Unit of measure must be between 2 and 100 characters")
     String unitOfMeasure,
 
     @NotNull(message = "Safety stock is required")
--- services/inventory-service/src/main/java/com/petek/inventory_service/product/ProductUpdateRequest.java
+++ services/inventory-service/src/main/java/com/petek/inventory_service/product/ProductUpdateRequest.java
@@ -3,13 +3,20 @@ package com.petek.inventory_service.product;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record ProductUpdateRequest (
     Long productId,
 
+    @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
     String productName,
+
     String description,
+
+    @Size(min = 2, max = 100, message = "Category must be between 2 and 100 characters")
     String category,
+
+    @Size(min = 2, max = 100, message = "Unit of measure must be between 2 and 100 characters")
     String unitOfMeasure,
 
     @Positive(message = "Safety stock should be positive")
