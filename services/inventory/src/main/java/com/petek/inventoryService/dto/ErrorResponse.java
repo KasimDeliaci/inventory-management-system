e0a0c0c70327938c86153fcc17ee360a3926f0a9
Eren GURELI <117351870+erengureli@users.noreply.github.com>
2025-09-04 08:17:26 +0300

    refactor(exception) (#35)
    * refactor(exception): Improve error handling and response structure
    
    * refactor(exception): remove redundant exception handler for runtime exceptions and change error details
    
    * refactor(exception): Improve exception handling in GlobalExceptionHandler.java

---
+++ services/inventory/src/main/java/com/petek/inventoryService/dto/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.petek.inventoryService.dto;
+
+import java.time.Instant;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Instant timestamp;
+    private Map<String, String> details;
+}
