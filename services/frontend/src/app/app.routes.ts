0d1d20a7ce071e9f7abda42eba20953a065d698d
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-07 15:27:18 +0300

    chore(app): register product routes and compose layout

--- services/frontend/src/app/app.routes.ts
+++ services/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product-page/product-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductPageComponent },
+  { path: '**', redirectTo: 'products' }
+];
