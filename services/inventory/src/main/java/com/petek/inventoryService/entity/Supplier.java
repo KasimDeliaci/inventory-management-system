f82064afba3f3dd7d995b36758180a710b928643
Eren GÜRELİ <erengureli10@gmail.com>
2025-09-09 10:57:07 +0300

    refactor(project): add one to many in product and supplier

--- services/inventory/src/main/java/com/petek/inventoryService/entity/Supplier.java
+++ services/inventory/src/main/java/com/petek/inventoryService/entity/Supplier.java
@@ -2,12 +2,15 @@ package com.petek.inventoryService.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.time.Instant;
+import java.util.List;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -44,6 +47,9 @@ public class Supplier {
     
     @Column(length = 50, nullable = false)
     private String city;
+
+    @OneToMany(mappedBy = "supplier", fetch = FetchType.LAZY)
+    private List<ProductSupplier> productSuppliers;
     
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
