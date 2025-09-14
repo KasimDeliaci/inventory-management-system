60d628226e45beaf7a0875c229fcb6eb937ef936
Eren GÜRELİ <erengureli10@gmail.com>
2025-09-14 11:16:37 +0300

    init(planning): init planning service

---
+++ services/planning/src/test/java/com/petek/planning/PlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petek.planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
